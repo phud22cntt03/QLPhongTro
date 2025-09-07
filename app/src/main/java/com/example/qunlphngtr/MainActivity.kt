b74f8c7eac708eff193ae8bede58fa18c3d8d678
phud22cntt03 <tphu27349@gmail.com>
2025-09-07 11:16:38 +0700

    update quản lý phòng activity

--- app/src/main/java/com/example/qunlphngtr/MainActivity.kt
+++ app/src/main/java/com/example/qunlphngtr/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.qunlphngtr
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // load giao diện activity_main.xml
+        val cardQuanLyPhongActivity = findViewById<MaterialCardView>(R.id.card1)
+        cardQuanLyPhongActivity.setOnClickListener {
+            val intent = Intent(this, QuanLyPhongActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
